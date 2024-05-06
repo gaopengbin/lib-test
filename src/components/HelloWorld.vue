40e72d893c345c08a03603bdaaa5024f74443ca1
laogao <1804287165@qq.com>
2024-05-06 11:18:11 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { initScene} from 'czm-sdk';
+import cc from 'czm-sdk';
+console.log(cc);
+const div: HTMLDivElement = document.createElement('div');
+div.id = 'earth';
+div.style.width = '80vw';
+div.style.height = '80vh';
+document.getElementById('app')?.appendChild(div);
+// document.body.appendChild(div);
+let viewer = initScene(div);
+console.log(viewer);
+</script>
+
+<template>
+  <!-- <div id="earth"></div> -->
+  <w-comp />
+</template>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    viteExternalsPlugin({
+      cesium: 'Cesium', // 外部化 cesium 依赖，之后全局访问形式是 window['Cesium']
+      // '@cesium/widgets': 'Cesium',
+    }),
+  ],
+  optimizeDeps:{
+    exclude: ['cesium']
+  }
+})
